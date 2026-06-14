4c1091fa9d92542d03f9bb01e405d8b3bea5f125
Sayooj S. Therattil <sayoojstherattil@gmail.com>
2026-06-14 15:09:50 +0530

    frontend and backend container Dockerfiles

---
+++ container-setup/ihms-frontend/npm_install.sh
@@ -0,0 +1,10 @@
+set -xe
+
+dirname=$1
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+cd $dirname
+npm i
---
+++ container-setup/ihms-frontend/npm_starter-frontend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+set -e
+
+dirname=$1
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+cd $dirname
+npm start
