a2bef733f010c7f6b42d562ff972423d8242365d
Sayooj S. Therattil <sayoojstherattil@gmail.com>
2026-06-26 16:00:33 +0530

    database run.sh complete
    the run.sh of database is able to fetch the necessary contents from the
    IHMS/.env file and use them to run the postgres container

--- container-setup/database-build-files/run.sh
+++ container-setup/database-build-files/run.sh
@@ -1,1 +1,22 @@
-podman run --rm -it --name ihms_database --pod chatapp --volume ihms_database:/var/lib/postgresql/18/main/ ihms_database:latest
+set -o errexit
+set -o pipefail
+set -o nounset
+
+database_name=$(grep -w 'database_name' ../../.env | awk -F '=' '{print $2}')
+database_password=$(grep -w 'database_password' ../../.env | awk -F '=' '{print $2}')
+
+database_container_name=$(grep -w 'database_container_name' ../../.env | awk -F '=' '{print $2}')
+database_container_image_name=$(grep -w 'database_container_image_name' ../../.env | awk -F '=' '{print $2}')
+database_container_volume_name=$(grep -w 'database_container_volume_name' ../../.env | awk -F '=' '{print $2}')
+
+pod_name=$(grep -w 'pod_name' ../../.env | awk -F '=' '{print $2}')
+
+podman run \
+	-it \
+	--rm  \
+	-e POSTGRES_DB=$database_name \
+	-e POSTGRES_PASSWORD=$database_password \
+	--name $database_container_name \
+	--pod $pod_name \
+	--volume $database_container_volume_name:/var/lib/postgresql/18/main/ \
+	$database_container_image_name:latest
