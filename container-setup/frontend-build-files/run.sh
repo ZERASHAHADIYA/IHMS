a4b1b1d1228322713dd4baead78cf93c9be93029
Sayooj S. Therattil <sayoojstherattil@gmail.com>
2026-06-26 16:06:42 +0530

    frontend run.sh complete
    the run.sh of frontend is able to fetch the necessary contents from the IHMS/.env file and use them to run the frontend container

--- container-setup/frontend-build-files/run.sh
+++ container-setup/frontend-build-files/run.sh
@@ -1,1 +1,17 @@
-podman run --rm -it --name ihms_frontend --pod chatapp --volume ihms_frontend:/root ihms_frontend:latest
+set -o errexit
+set -o pipefail
+set -o nounset
+
+frontend_container_name=$(grep -w 'frontend_container_name' ../../.env | awk -F '=' '{print $2}')
+frontend_container_image_name=$(grep -w 'frontend_container_image_name' ../../.env | awk -F '=' '{print $2}')
+frontend_container_volume_name=$(grep -w 'frontend_container_volume_name' ../../.env | awk -F '=' '{print $2}')
+
+pod_name=$(grep -w 'pod_name' ../../.env | awk -F '=' '{print $2}')
+
+podman run \
+	-it \
+	--rm  \
+	--name $frontend_container_name \
+	--pod $pod_name \
+	--volume $frontend_container_volume_name:/root/ \
+	$frontend_container_image_name:latest
