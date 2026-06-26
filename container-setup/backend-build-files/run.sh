0963454f97afa4d9164a7c3206ad74f76fd0e87d
Sayooj S. Therattil <sayoojstherattil@gmail.com>
2026-06-26 16:10:24 +0530

    backend run.sh complete
    the run.sh of backend is able to fetch the necessary contents from the IHMS/.env file and use them to run the backend container

--- container-setup/backend-build-files/run.sh
+++ container-setup/backend-build-files/run.sh
@@ -1,1 +1,17 @@
-podman run --rm -it --name ihms_backend --pod chatapp --volume ihms_backend:/root ihms_backend:latest
+set -o errexit
+set -o pipefail
+set -o nounset
+
+backend_container_name=$(grep -w 'backend_container_name' ../../.env | awk -F '=' '{print $2}')
+backend_container_image_name=$(grep -w 'backend_container_image_name' ../../.env | awk -F '=' '{print $2}')
+backend_container_volume_name=$(grep -w 'backend_container_volume_name' ../../.env | awk -F '=' '{print $2}')
+
+pod_name=$(grep -w 'pod_name' ../../.env | awk -F '=' '{print $2}')
+
+podman run \
+	-it \
+	--rm  \
+	--name $backend_container_name \
+	--pod $pod_name \
+	--volume $backend_container_volume_name:/root/ \
+	$backend_container_image_name:latest
