aee8d23448f833f08df8d4449a8248fd5673b4f3
Sayooj S. Therattil <sayoojstherattil@gmail.com>
2026-06-28 12:47:17 +0530

    check debian stable image before proceeding
    otherwise, all three Dockerfiles would pull the image at the same time as container building is done in parallel

--- container-setup/setup.sh
+++ container-setup/setup.sh
@@ -2,6 +2,27 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+log() {
+	echo "log: $1"
+	sleep 1
+}
+
+log 'checking whether a debian stable image exists'
+set +o errexit
+podman image exists debian:stable 
+
+if [ $? -eq 0 ]
+then
+	set -o errexit
+	log 'found a debian stable image'
+else
+	set -o errexit
+	log 'did not find a debian stable image. pulling before proceeding.'
+	podman pull docker.io/library/debian:stable
+fi
+
+log 'proceeding with the setup'
+
 source ./generate_env.sh
 source ./build_containers.sh
 source ./create_volumes.sh
